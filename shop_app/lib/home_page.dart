17b7542f94a1a3ca8e87c38be6b546b74370963c
Nistha <codernistha27@gmail.com>
2024-02-02 19:25:25 +0530

    products added

--- shop_app/lib/home_page.dart
+++ shop_app/lib/home_page.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
-  
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<String>filters  = const [
     'All',
     'Adidas',
     'Nike',
     'Puma'
   ];
+ late String selectedFilter = filters[0];
+ 
+  @override
+  void initState()
+  {
+    super.initState();
+    selectedFilter = filters[0];
+  }
   @override
   Widget build(BuildContext context) {
     const border = OutlineInputBorder(
@@ -22,9 +35,9 @@ class HomePage extends StatelessWidget {
       body:SafeArea(
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
-             const Padding(
+              Padding(
                 padding: EdgeInsets.all(20.0),
                 child: Text(
                   'Shoes\nCollection', 
@@ -34,7 +47,7 @@ class HomePage extends StatelessWidget {
                     )
                   ),
                 ),
-                const Expanded(child:
+                 Expanded(child:
                  TextField(
                   decoration: InputDecoration(
                     hintText: 'Search', 
@@ -45,19 +58,54 @@ class HomePage extends StatelessWidget {
                   ), 
                  )
                 ),
-                ListView.builder(
-                  itemCount : filters.length,
-                  itemBuilder: (context , index) {
-                    final filter = filters[index];
-                    return Chip(
-                      label: Text(filter),);
-                  },
-                )
               ],
-            ) 
+            ),
+            SizedBox(
+              height : 120,
+              child: ListView.builder(
+                    itemCount : filters.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context , index) {
+                      final filter = filters[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal:8.0),
+                        child: GestureDetector(
+                          onTap :() {
+                              setState(() { 
+                              selectedFilter = filter;
+                              });
+                          },  
+                          child: Chip(
+                            backgroundColor :selectedFilter == filter 
+                            ? Theme.of(context).colorScheme.primary
+                            :  const Color.fromRGBO(245,247, 249, 1),
+                            side: const BorderSide(
+                              color: Color.fromRGBO(245, 247, 249, 1)
+                            ),
+                            label: Text(filter),
+                            labelStyle: const TextStyle(
+                             fontSize: 14,
+                            ),
+                            padding : const EdgeInsets.symmetric(
+                              horizontal:13 ,  
+                              vertical : 10
+                            ),
+                          shape:RoundedRectangleBorder(
+                          borderRadius:  BorderRadius.circular(30)
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context , index) {}
+               ),
           ],
         ), 
      )
-   );  
+   );
  }
 }
